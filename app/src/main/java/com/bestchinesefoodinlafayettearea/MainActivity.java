be41fd3c4988063d5871fbb44b43863b3df69a7c
tim1800 <pandengchina@gmail.com>
2017-05-14 21:58:16 -0400

    ...

---
+++ app/src/main/java/com/bestchinesefoodinlafayettearea/MainActivity.java
@@ -0,0 +1,65 @@
+package com.bestchinesefoodinlafayettearea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.btnFood1);
+        Button button2 = (Button) findViewById(R.id.btnFood2);
+        Button button3 = (Button) findViewById(R.id.btnFood3);
+        Button button4 = (Button) findViewById(R.id.btnFood4);
+        Button button5 = (Button) findViewById(R.id.btnFood5);
+        Button button6 = (Button) findViewById(R.id.btnFood6);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail1.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail2.class));
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail3.class));
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail4.class));
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail5.class));
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Detail6.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/bestchinesefoodinlafayettearea/SplashActivity.java
+++ app/src/main/java/com/bestchinesefoodinlafayettearea/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.bestchinesefoodinlafayettearea;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task, 2000);
     }
 }
